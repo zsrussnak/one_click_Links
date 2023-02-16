db1ed2d8563f18d5f882f243eb66e8ae7b6f54f1
Zsolt Russnak <zsoltrussnak@gmail.com>
2023-02-16 05:07:20 +0000

    Config.astro branch changed from master to main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,52 @@
-import { defineConfig } from "astro/config"
-import NetlifyCMS from "astro-netlify-cms"
+import { defineConfig } from "astro/config";
+import NetlifyCMS from "astro-netlify-cms";
 
 // <https://astro.build/config>
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
 
 // <https://astro.build/config>
-import netlify from "@astrojs/netlify/functions"
+import netlify from "@astrojs/netlify/functions";
 
 // <https://astro.build/config>
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    NetlifyCMS({
-      config: {
-        backend: {
-          name: "git-gateway",
-          branch: "master",
-        },
-        collections: [
-          {
-            label: "Links",
-            name: "links",
+	integrations: [
+		tailwind(),
+		NetlifyCMS({
+			config: {
+				backend: {
+					name: "git-gateway",
+					branch: "main",
+				},
+				collections: [
+					{
+						label: "Links",
+						name: "links",
 						slug: "{{linkTitle}}",
-            folder: "src/pages/links",
-            create: true,
-            delete: true,
-            fields: [
-              {
-                label: "Link Title",
-                name: "linkTitle",
-                widget: "string",
-              },
-              {
-                label: "Link Description",
-                name: "linkDescription",
-                widget: "string",
-              },
-              {
-                label: "Link URL",
-                name: "linkURL",
-                widget: "string",
-              },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
-  output: "server",
-  adapter: netlify(),
-})
+						folder: "src/pages/links",
+						create: true,
+						delete: true,
+						fields: [
+							{
+								label: "Link Title",
+								name: "linkTitle",
+								widget: "string",
+							},
+							{
+								label: "Link Description",
+								name: "linkDescription",
+								widget: "string",
+							},
+							{
+								label: "Link URL",
+								name: "linkURL",
+								widget: "string",
+							},
+						],
+					},
+				],
+			},
+		}),
+	],
+	output: "server",
+	adapter: netlify(),
+});
