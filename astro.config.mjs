b4723fdccfbb3cb5773c3e228afa9a9ae7be150a
Zsolt Russnak <zsoltrussnak@gmail.com>
2023-02-16 04:46:10 +0000

    Setup Netlify CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,52 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import NetlifyCMS from "astro-netlify-cms"
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+// <https://astro.build/config>
+import tailwind from "@astrojs/tailwind"
 
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+// <https://astro.build/config>
+import netlify from "@astrojs/netlify/functions"
 
-// https://astro.build/config
+// <https://astro.build/config>
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "master",
+        },
+        collections: [
+          {
+            label: "Links",
+            name: "links",
+						slug: "{{linkTitle}}",
+            folder: "src/pages/links",
+            create: true,
+            delete: true,
+            fields: [
+              {
+                label: "Link Title",
+                name: "linkTitle",
+                widget: "string",
+              },
+              {
+                label: "Link Description",
+                name: "linkDescription",
+                widget: "string",
+              },
+              {
+                label: "Link URL",
+                name: "linkURL",
+                widget: "string",
+              },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
   output: "server",
-  adapter: netlify()
-});
+  adapter: netlify(),
+})
---
+++ src/pages/links/twitter.md
@@ -0,0 +1,5 @@
+---
+linkTitle: Twitter
+linkDescription: Follow Me on Twitter
+linkURL: https://www.twitter.com/@ZsoltRussnak
+---
